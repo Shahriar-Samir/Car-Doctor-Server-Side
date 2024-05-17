11d6c4f8712baf43760646eecfd975312b818d5a
Shahriar-Samir <shabusiness035@gmail.com>
2024-05-18 02:02:40 +0600

    added services get route

--- index.js
+++ index.js
@@ -4,9 +4,7 @@ const app = express()
 const port = process.env.PORT || 5000
 require('dotenv').config()
 
-app.get('/', (req,res)=>{
-        res.send("Car Doctor Server Side")
-})
+
 
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
@@ -24,19 +22,33 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
+
+    app.get('/', (req,res)=>{
+        res.send("Car Doctor Server Side")
+    })
+
+    const serviceCollection = client.db('Car_Doctor').collection('Services')
+
+    app.get('/services',async (req,res)=>{
+        const result = await serviceCollection.find().toArray()
+        res.send(result)
+    })
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
 
 
 
-app.listen(()=>{
+app.listen(port,()=>{
     console.log(`listening on port ${port}`)
 })
